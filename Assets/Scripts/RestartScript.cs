8eb99ac642617366c2e331ff3a23e6550d002105
ryderhanson <ryder.hanson@gmail.com>
2018-07-06 00:39:09 -0700

    first draft build

---
+++ Assets/Scripts/RestartScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.visible = false;
+
+        Invoke("ResetScene", 40f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
